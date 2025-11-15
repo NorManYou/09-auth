8a2d91a4090d4c7f3b6f06f3fb1b00d2f124acd8
Vitalii Shumakov <fafde8120@gmail.com>
2025-11-15 15:51:43 +0100

    add login

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { api } from "../../api";
 import { cookies } from "next/headers";
+
 import { parse } from "cookie";
 import { isAxiosError } from "axios";
 import { logErrorResponse } from "../../_utils/utils";
--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -5,6 +5,7 @@ import { parse } from "cookie";
 import { isAxiosError } from "axios";
 import { logErrorResponse } from "../../_utils/utils";
 
+
 export async function GET() {
   try {
     const cookieStore = await cookies();
